b641ce815dde3aaf658730022360cf041e537725
ITZTLI <154475063+ITZTLI-Music@users.noreply.github.com>
2025-02-23 23:10:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -42,3 +42,12 @@ function showLyricsButton(songIndex) {
 document.getElementById("bandcamp-player").addEventListener("mouseenter", function () {
     showLyricsButton(0); // Simulating Song 1 starting
 });
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
